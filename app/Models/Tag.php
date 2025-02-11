36478b53cec401e115b9cfa8d42fe5650da00405
benjamin <benjaminpiscart@gmail.com>
2025-02-11 14:25:23 +0100

    fix tag name replace by title

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 class Tag extends Model
 {
     protected $fillable = [
-        'name',
+        'title',
     ];
 
     public function books(): BelongsToMany
