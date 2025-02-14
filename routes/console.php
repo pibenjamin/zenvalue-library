2b7e4b4cfb386a5e0a688458dfae5d108dcf158f
benjaminpi <benjaminpiscart@gmail.com>
2025-02-14 23:47:54 +0100

    fix

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Artisan;
-use Illuminate\Console\Scheduling\Schedule;
 use App\Console\Commands\SendLoanReminders;
+use Illuminate\Support\Facades\Schedule;
 
 
 Schedule::command('loans:send-reminders')->dailyAt('09:00');
