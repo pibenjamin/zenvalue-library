a1d6628ea02de3974639d7e93df2c43b805dfe6d
benjaminpi <benjaminpiscart@gmail.com>
2025-02-20 10:43:53 +0100

    clean tags

--- app/Console/Commands/CleanTags.php
+++ app/Console/Commands/CleanTags.php
@@ -4,6 +4,8 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 use App\Models\Tag;
+use Illuminate\Support\Str;
+
 class CleanTags extends Command
 {
     /**
@@ -18,19 +20,23 @@ class CleanTags extends Command
      *
      * @var string
      */
-    protected $description = 'Command description';
+    protected $description = 'Nettoyage des tags';
 
     /**
      * Execute the console command.
      */
     public function handle()
     {
-        // Récupérer tous les tags et transforme les en mots en minuscule
+        // Récupérer tous les tags et transforme les en mots en minuscule et joute des ->info()
         $tags = Tag::all();
         foreach ($tags as $tag) {
-            $tag->name = strtolower($tag->name);
-            $tag->slug = strtolower($tag->name);
+
+            $this->info($tag->title);
+            $tag->title = strtolower($tag->title);
+            $tag->slug = Str::slug($tag->title);
             $tag->save();
+            $this->info('Tag nettoyé');
         }
+        $this->info('Nettoyage des tags terminé ('.count($tags).' tags nettoyés)');
     }
 }
