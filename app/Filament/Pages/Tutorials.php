13c15b1fd90a6d6134a40494c61e795e2acbf3ae
benjaminpi <benjaminpiscart@gmail.com>
2025-03-06 11:21:10 +0100

    video tuto

---
+++ app/Filament/Pages/Tutorials.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Filament\Pages;
+
+use Filament\Pages\Page;
+
+class Tutorials extends Page
+{
+    protected static ?string $navigationLabel   = 'Tutoriels';
+    protected static string $view               = 'filament.pages.tutorials';
+    protected static ?string $navigationGroup   = 'Support & Ressources';
+    protected static ?int $navigationSort       = 3;
+    protected static ?string $navigationIcon    = 'heroicon-o-video-camera';
+    protected static ?string $title             = 'Tutoriels vidéo';
+
+
+    protected function getHeaderWidgets(): array
+    {
+        return [
+
+
+        ];
+    }
+
+}
---
+++ resources/views/filament/pages/tutorials.blade.php
@@ -0,0 +1,26 @@
+<x-filament-panels::page>
+    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        <!-- Carte Tutoriel 1 -->
+        <div class="bg-white dark:bg-gray-800 rounded-lg shadow-md overflow-hidden">
+            <div class="plyr__video-embed" id="player">
+                <video 
+                    id="player"
+                    controls
+                    class="w-full h-full"
+                >
+                    <source src="/storage/video/activate-existing-acount.mp4" type="video/mp4">
+                </video>
+            </div>
+            <div class="p-4">
+                <h3 class="text-lg font-semibold mb-2">Activer un compte citizen déjà existant</h3>
+                <p class="text-gray-600 dark:text-gray-300 text-sm">
+                    Si vous avez rejoins Zen Value avant le <span class="inline-block w-fit"><x-filament::badge color="danger">15 février 2025</x-filament::badge></span>, vous pouvez activer votre compte citizen en suivant les étapes ci-dessous.
+                </p>
+            </div>
+        </div>
+    </div>
+</x-filament-panels::page>
+
+<script>
+    const player = new Plyr('#player');
+</script>
