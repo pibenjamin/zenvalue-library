9988b9eeb498e64ef41f8c14a495ef20e07e97ee
Benjamin Piscart <benjaminpiscart@gmail.com>
2025-04-23 14:22:43 +0200

    Update BookTrainingsWidget description and route for editing books
    - Revised the description in the BookTrainingsWidget to clarify that the bibliography may include books not yet in the catalog and encouraged users to report any desired acquisitions.
    - Changed the route for super_admin users when editing books to ensure it points to the correct resource.

--- app/Filament/Resources/TrainingResource/Widgets/BookTrainingsWidget.php
+++ app/Filament/Resources/TrainingResource/Widgets/BookTrainingsWidget.php
@@ -35,13 +35,13 @@ class BookTrainingsWidget extends BaseWidget
                     //->where('books.status', Book::STATUS_ON_SHELF)
             )
             ->heading('Les livres liés et/ou permettant d\'approfondir cette formation')
-            ->description('Si les informations divergent à propos du listing des livres, c\'est certainement parce qu\'un livre est manquant dans la bibliothèque ou n\'est pas encore totalement qualifié dans le catalogue.')
+            ->description('Cette bibliographie peut contenir des livres qui ne sont pas encore dans notre catalogue. Si vous souhaitez les acquérir, veuillez nous le signaler.')
             ->columns([
                 Tables\Columns\TextColumn::make('title')
                     ->label('Livre')
                     ->url(function (Book $record) {
                         if (auth()->user()->hasRole('super_admin')) {
-                            return route('filament.admin.resources.books.edit', $record->book_id);
+                            return route('filament.admin.resources.book-admin.edit', $record->book_id);
                         }
                         return route('filament.admin.resources.books.view', $record->book_id);
                     })
